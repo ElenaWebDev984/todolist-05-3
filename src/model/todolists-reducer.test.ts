29c2644a3664a8d812b02f8b79bf502f3a670c1d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-05-16 20:50:30 +0200

    add beforeEach

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -1,38 +1,31 @@
 import {v1} from "uuid";
-import {expect, test} from 'vitest'
+import {beforeEach, expect, test} from 'vitest'
 import type {TodolistType} from "../App.tsx";
 import {createTodolistAC, deleteTodolistAC, todolistsReducer} from "./todolists-reducer.ts";
 
-test('correct todolist should be deleted', () => {
-    const todolistId1 = v1()
-    const todolistId2 = v1()
+let todolistId1: string
+let todolistId2: string
+let startState: TodolistType[] = []
 
-    // 1. Стартовый state
-    const startState: TodolistType[] = [
+beforeEach(() => {
+    todolistId1 = v1()
+    todolistId2 = v1()
+
+    startState = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
         {id: todolistId2, title: 'What to buy', filter: 'all'},
     ]
+})
 
-    // 2. Действие
+test('correct todolist should be deleted', () => {
     const endState = todolistsReducer(startState, deleteTodolistAC(todolistId1))
 
-    // 3. Проверка, что действие изменило state соответствующим образом
-    // в массиве останется один тудулист
     expect(endState.length).toBe(1)
-    // удалится нужный тудулист, не любой
     expect(endState[0].id).toBe(todolistId2)
 })
 
 
 test('correct todolist should be created', () => {
-    const todolistId1 = v1()
-    const todolistId2 = v1()
-
-    const startState: TodolistType[] = [
-        {id: todolistId1, title: 'What to learn', filter: 'all'},
-        {id: todolistId2, title: 'What to buy', filter: 'all'},
-    ]
-
     const title = 'New todolist'
     const endState = todolistsReducer(startState, createTodolistAC(title))
 
