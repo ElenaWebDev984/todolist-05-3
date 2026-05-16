34b15ef543410bd10b14b6c0c37cd4e1301ad684
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-05-16 14:39:10 +0200

    add  test for deleteTodolist

---
+++ src/model/todolists-reducer.test.ts
@@ -0,0 +1,39 @@
+import {v1} from "uuid";
+import {expect, test} from 'vitest'
+import type {TodolistType} from "../App.tsx";
+import {todolistsReducer} from "./todolists-reducer.ts";
+
+test('correct todolist should be deleted', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    // 1. Стартовый state
+    const startState: TodolistType[] = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+
+    // 2. Действие
+    // TODO variant 1 of typification
+    // const action: DeleteTodolistActionType = {
+    //     type: 'delete_todolist',
+    //     payload: {
+    //         id: todolistId1
+    //     }
+    // }
+    // TODO variant 2 of typification
+    const action = {
+        type: 'delete_todolist' as const,
+        payload: {
+            id: todolistId1
+        }
+    }
+    const endState = todolistsReducer(startState, action)
+
+    // 3. Проверка, что действие изменило state соответствующим образом
+    // в массиве останется один тудулист
+    expect(endState.length).toBe(1)
+    // удалится нужный тудулист, не любой
+    expect(endState[0].id).toBe(todolistId2)
+
+})
